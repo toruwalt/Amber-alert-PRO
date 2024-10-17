8906cd6104ce17dc7c56f548ff1e5bb85dde9821
Torubiri Walter <toruwalt1997@gmail.com>
2024-10-18 00:33:53 +0100

    UserModel done

---
+++ backend/models/userModel.js
@@ -0,0 +1,18 @@
+import { clear } from 'console';
+import mongoose from 'mongoose';
+const { randomUUID } = require('crypto');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {type: String,required: true,unique: true},
+    email: {type: String,required: true,unique: true},
+    password: {type: String,required: true},
+    firstname: {type: String,required: true},
+    lastname: {type: String,required: true},
+    phone: {type: Number,required: true,unique: true},
+    image: {type: String,required: false},
+    role: {type: String,default: "user"},
+    createdAt: {type: Date,default: Date.now},
+    updatedAt: {type: Date,default: Date.now}
+    },
+    {timestamps: true});
