c8941ce240f3de08192c08f66eef5dea4da550af
Torubiri Walter <toruwalt1997@gmail.com>
2024-10-18 00:40:44 +0100

    UserModel done:Attempt 2

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,15 +4,14 @@ const { randomUUID } = require('crypto');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    username: {type: String,required: true,unique: true},
-    email: {type: String,required: true,unique: true},
-    password: {type: String,required: true},
-    firstname: {type: String,required: true},
-    lastname: {type: String,required: true},
-    phone: {type: Number,required: true,unique: true},
-    image: {type: String,required: false},
-    role: {type: String,default: "user"},
-    createdAt: {type: Date,default: Date.now},
-    updatedAt: {type: Date,default: Date.now}
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+    phone: {type: Number, required: true, unique: true},
+    image: {type: String, required: false},
+    role: {type: String, default: "user"}
     },
-    {timestamps: true});
+    {timestamps: true}
+);
